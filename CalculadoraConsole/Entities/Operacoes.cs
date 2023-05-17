36fbfa79de87d6e5b243dc13446669922544fb12
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-17 19:22:41 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraConsole/Entities/Operacoes.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace CalculadoraConsole.Entities
+{
+    internal class Operacoes
+    {
+        public int Resultado { get; set; }
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+
+        public void Soma()
+        {
+            Console.Write("Insira 2 valores para soma: ");
+            string[] valores = Console.ReadLine().Split(" ");
+            Valor1 = int.Parse(valores[0]);
+            Valor2 = int.Parse(valores[1]);
+            Resultado = Valor1 + Valor2;
+        }
+        public void Menos()
+        {
+            Console.Write("Insira 2 valores para subtrair: ");
+            string[] valores = Console.ReadLine().Split(" ");
+            Valor1 = int.Parse(valores[0]);
+            Valor2 = int.Parse(valores[1]);
+            Resultado = Valor1 - Valor2;
+        }
+
+        public void Dividir()
+        {
+            Console.Write("Insira 2 valores para dividir: ");
+            string[] valores = Console.ReadLine().Split(" ");
+            Valor1 = int.Parse(valores[0]);
+            Valor2 = int.Parse(valores[1]);
+            Resultado = Valor1 / Valor2;
+            if (Resultado % 2 != 0)
+            {
+                Console.WriteLine("valor dado não é divisivel");
+                Dividir();
+            }
+        }
+
+        public void Multiplicar()
+        {
+            Console.Write("Insira 2 valores para multiplicar: ");
+            string[] valores = Console.ReadLine().Split(" ");
+            Valor1 = int.Parse(valores[0]);
+            Valor2 = int.Parse(valores[1]);
+            Resultado = Valor1 * Valor2;
+        }
+
+
+        public override string ToString()
+        {
+            return $"O resultado da sua operação é: {Resultado}";
+        }
+
+
+    }
+}
---
+++ CalculadoraConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using CalculadoraConsole.Entities;
+
+namespace CalculadoraConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool terminou = false;
+
+
+            Operacoes operacoes = new Operacoes();
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - multiplicação");
+            Console.WriteLine("qualquer outro botão para sair");
+            Console.WriteLine();
+
+            Console.Write("insira o numero da operação que deseja: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (n)
+            {
+                case 1:
+                    {
+                        operacoes.Soma();
+                        Console.WriteLine(operacoes);
+                        break;
+                    }
+                case 2:
+                    {
+                        operacoes.Menos();
+                        Console.WriteLine(operacoes);
+                        break;
+                    }
+                case 3:
+                    {
+                        operacoes.Dividir();
+                        Console.WriteLine(operacoes);
+                        break;
+                    }
+                case 4:
+                    {
+                        operacoes.Multiplicar();
+                        Console.WriteLine(operacoes);
+                        break;
+                    }
+                    default:
+                    {
+                        break;
+                    }
+            }
+
+
+        }
+    }
+}
